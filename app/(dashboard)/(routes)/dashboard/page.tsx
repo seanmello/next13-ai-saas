608084dae319303836d4126f7bbe024a99b25104
SeanMello <127159049+seanmello@users.noreply.github.com>
2023-09-28 00:02:02 -0400

    Update page.tsx

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
+          HAVE A LITTLE TALK WITH AI
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Chat with the smartest AI - Experience the power of AI
