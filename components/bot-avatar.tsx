f8607a15ec210b32287af624bd47529a59eb99df
SeanMello <127159049+seanmello@users.noreply.github.com>
2023-09-27 23:32:09 -0400

    Update bot-avatar.tsx

--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const BotAvatar: React.FC = () => {
   return (
     <div className="bot-avatar">
-      <img src="/path/to/avatar.png" alt="Bot Avatar" className="w-16 h-16 rounded-full" />
+      <img src="/baby boy!-2.png" alt="Bot Avatar" className="w-16 h-16 rounded-full" />
     </div>
   );
 };
