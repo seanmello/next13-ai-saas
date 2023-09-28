c481be8e5e8fea3883f107701ef3d9bf57afe579
SeanMello <127159049+seanmello@users.noreply.github.com>
2023-09-27 20:26:35 -0400

    Update landing-navbar.tsx

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Montserrat } from "next/font/google";
-import Image from "next/image";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
 import { useAuth } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
@@ -17,16 +17,16 @@ export const LandingNavbar = () => {
     <nav className="p-4 bg-transparent flex items-center justify-between">
       <Link href="/" className="flex items-center">
         <div className="relative h-8 w-8 mr-4">
-          <Image fill alt="Logo" src="https://media.discordapp.net/attachments/1062513593284829366/1156737582936375347/smalltalk2.gif?ex=65160f52&is=6514bdd2&hm=75cc5a077cee85739d182d6a6166e65b0ded3df93038dbd2bea032a89b927d58&=&width=584&height=584" />
+          <Image fill alt="Logo" src="/smalltalk2.gif" />
         </div>
         <h1 className={cn("text-2xl font-bold text-white", font.className)}>
-          Small Talk
+          Genius
         </h1>
       </Link>
       <div className="flex items-center gap-x-2">
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button variant="outline" className="rounded-full">
-            Get TALKING....
+            Get Started
           </Button>
         </Link>
       </div>
