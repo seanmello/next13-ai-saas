5eb2761eb6f8aed120ce27213ab70fbcee4a6d6b
SeanMello <127159049+seanmello@users.noreply.github.com>
2023-09-27 20:07:11 -0400

    1

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -20,13 +20,13 @@ export const LandingNavbar = () => {
           <Image fill alt="Logo" src="https://media.discordapp.net/attachments/1062513593284829366/1156737582936375347/smalltalk2.gif?ex=65160f52&is=6514bdd2&hm=75cc5a077cee85739d182d6a6166e65b0ded3df93038dbd2bea032a89b927d58&=&width=584&height=584" />
         </div>
         <h1 className={cn("text-2xl font-bold text-white", font.className)}>
-          Genius
+          Small Talk
         </h1>
       </Link>
       <div className="flex items-center gap-x-2">
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button variant="outline" className="rounded-full">
-            Get Started
+            Get TALKING....
           </Button>
         </Link>
       </div>
--- components/loader.tsx
+++ components/loader.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export const Loader = () => {
   return (
@@ -6,12 +6,12 @@ export const Loader = () => {
       <div className="w-10 h-10 relative animate-spin">
         <Image
           alt="Logo"
-          src="/logo.png"
+          src="https://media.discordapp.net/attachments/1062513593284829366/1156737582936375347/smalltalk2.gif?ex=65160f52&is=6514bdd2&hm=75cc5a077cee85739d182d6a6166e65b0ded3df93038dbd2bea032a89b927d58&=&width=584&height=584"
           fill
         />
       </div>
       <p className="text-sm text-muted-foreground">
-        Genius is thinking...
+        Conversation Loading...
       </p>
     </div>
   );
