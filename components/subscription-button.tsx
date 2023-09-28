85d8de3c89b2dea6b8e8255e0bd11c942b8dcbf0
SeanMello <127159049+seanmello@users.noreply.github.com>
2023-09-27 22:14:05 -0400

    Update subscription-button.tsx

--- components/subscription-button.tsx
+++ components/subscription-button.tsx
@@ -22,7 +22,7 @@ export const SubscriptionButton = ({
 
       window.location.href = response.data.url;
     } catch (error) {
-      toast.error("Something went wrong");
+      toast.error("Something went wrong...Or your TALK was too CHEAP...lol");
     } finally {
       setLoading(false);
     }
