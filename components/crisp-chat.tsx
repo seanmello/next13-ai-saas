2a596fbcc3162b05f34a86c3585686efb55cd4f1
SeanMello <127159049+seanmello@users.noreply.github.com>
2023-09-28 20:49:57 -0400

    Update crisp-chat.tsx

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -4,14 +4,9 @@ import { useEffect } from "react";
 import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
-  <script
-  src="https://widget.yourgpt.ai/script.js" 
-  id="yourgpt-chatbot" 
-  data-config='{
-  "YOURGPT_CHATBOT_UID": "21dcba3e-5227-48ac-a3c5-5583077d399c", 
-  "YOURGPT_WIDGET_UID": "da535d4b-d36e-4731-93a2-ef7b1288f46c"
-  }'>
-</script>    
+useEffect(() => {
+    Crisp.configure("d5fd7593-8f0b-4a00-9ed0-4d623fb02952");
+  }, []);
 
-  
+  return null;  
 };
