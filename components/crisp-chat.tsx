cfe76c3ebe40ebce0a40d0c6210f64cd845928bf
SeanMello <127159049+seanmello@users.noreply.github.com>
2023-09-28 20:40:39 -0400

    Update crisp-chat.tsx

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -4,9 +4,14 @@ import { useEffect } from "react";
 import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
-  useEffect(() => {
-    Crisp.configure("21dcba3e-5227-48ac-a3c5-5583077d399c");
-  }, []);
+  <script
+  src="https://widget.yourgpt.ai/script.js" 
+  id="yourgpt-chatbot" 
+  data-config='{
+  "YOURGPT_CHATBOT_UID": "21dcba3e-5227-48ac-a3c5-5583077d399c", 
+  "YOURGPT_WIDGET_UID": "da535d4b-d36e-4731-93a2-ef7b1288f46c"
+  }'>
+</script>    
 
-  return null;
+  
 };
