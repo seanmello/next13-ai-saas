8bf798c7cf3bb12dfb4e1a5e84ca60e7eb308ee6
SeanMello <127159049+seanmello@users.noreply.github.com>
2023-10-05 16:52:05 -0400

    Update crisp-provider.tsx

--- components/crisp-provider.tsx
+++ components/crisp-provider.tsx
@@ -1,20 +1,6 @@
-import { CrispChat } from "@/components/crisp-chat";
+"use client";
 
-const Testimonials = () => {
-  // Testimonials code here...
-};
+ import { CrispChat } from "@/components/crisp-chat";
 
-const HomePage = () => {
-  return (
-    <div>
-      <use-client />
-      <CrispProvider />
-      <Testimonials />
-      <a href="https://dust.tt/w/a2354950ab/assistant/4771371b76" className="text-center block mt-8 text-blue-500 hover:text-blue-700">
-        Learn More
-      </a>
-    </div>
-  );
-};
-
-export default HomePage;
+ export const CrispProvider = () => {
+   return <CrispChat />
